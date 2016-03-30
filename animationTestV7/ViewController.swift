4fc4390fda9dcb34c3affd6dc10080df06d4d228
Prashant Rathod <Prashant@prashants-imac.local>
2016-03-30 11:01:29 +0100

    Initial Commit

---
+++ animationTestV7/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  animationTestV7
+//
+//  Created by Prashant Rathod on 30/03/2016.
+//  Copyright © 2016 PrashantRathod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
